846a406b3189c3098e6dd5111b069571b0e189c7
Privalov94 <arprivalov110594@gmail.com>
2022-05-22 15:39:37 +0500

    first commit variables

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,14 @@
-function compareArrays(arr1, arr2) {
-  let result;
+'use strict'
 
-  // Ваш код
+function compareArrays(arr1, arr2) {
+
+  return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index])
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter((value) => value > 0).filter((value) => value % 3 === 0).map((value) => value * 10)
 
   // Ваш код
 
