bfda42e286547f7e0c037eabb3f1e0504cd5da12
Privalov94 <arprivalov110594@gmail.com>
2022-05-25 12:10:08 +0500

    first commit variables

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,52 @@
+'use strict'
+
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
+let studentFirst = new Student("Tony", "male", 37)
+ 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
+
+Student.prototype.addMark = function (mark) {
+
+  if ( this.marks === undefined) {
+     this.marks = [mark]; // Первая оценка добавляется в массив
+    } else {
+     this.marks.push(mark); // Все остальные оценки пушатся
+    }
+}
+
+Student.prototype.addMarks = function (mark1, mark2,  mark3, ...rest) {
+  if ( this.marks === undefined) {
+    this.marks = [mark1, mark2, mark3];
+   } else {
+    this.marks.push(mark1, mark2, mark3);
+   }
+}
+
+
+Student.prototype.getAverage = function() {
+  return this.marks.reduce((mark1, mark2, mark3) => (mark1 + mark2 + mark3) / this.marks.length)
+}
+
+Student.prototype.exclude = function (reason) {
+   delete this.subject;
+   delete this.marks;
+   this.exclude = reason;
+}
+
+
+   
+
+
+
+
+
+
 
 // ваш код для остальных методов
