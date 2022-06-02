819efe27aaf91ce353ae0875911e794f227338c8
Privalov94 <arprivalov110594@gmail.com>
2022-06-02 08:39:36 +0500

    first commit variables

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -4,21 +4,16 @@ function parseCount(number) {
     const parsedNumber = Number.parseInt(number);
     if (isNaN(parsedNumber)) {
         throw new Error ("Невалидное значение");
-    } else {
+    } 
         return parsedNumber;
-    }
 }
 
 function validateCount(values) {
     try {
         parseCount(values);
         const parsedValues = Number.parseInt(values);
-        if (isNaN(parsedValues)) {
-            throw new Error ("Невалидное значение");
-        } else {
-            return parsedValues;
-        }
-        
+        return parsedValues;
+         
     }  catch (err) {
         return err;
     }
@@ -30,14 +25,8 @@ class Triangle {
         this.b = b;
         this.c = c;
 
-        if (this.a + this.b > this.c) {
+        if ((a + b > c) || (a + c > b) || (b + c > a)) {
             throw new Error("Треугольник с такими сторонами не существует");
-        }
-        if(this.a + this.c > this.b) {
-            throw new Error("Треугольник с такими сторонами не существует");
-        } 
-        if(this.b + this.c > this.a) {
-            throw new Error("Треугольник с такими сторонами не существует"); 
         }
     }
     
@@ -47,7 +36,7 @@ class Triangle {
     }
 
     getArea () {
-        let p = (this.a + this.b + this.c) * 0.5;
+        let p = sum / 2 ;
         let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
         s = s.toFixed(3);
         s = Number(s);
@@ -67,9 +56,9 @@ function getTriangle(a, b, c) {
             }, getPerimeter() {
                 return "Ошибка! Треугольник не существует"; 
             }
-          }
         }
-      }      
+    }
+}      
      
  
 
